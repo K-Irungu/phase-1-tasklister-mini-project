13e2e3b20b2970dbe90f6f1eddefab3b66bb54d5
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-08-28 23:36:50 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createToDo(e.target.newtaskdescription.value);
+    form.reset();
+  });
+
 });
+
+function createToDo(todo) {
+  console.log(todo);
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'x';
+  p.textContent = `${todo}  `;
+  p.appendChild(btn);
+  document.querySelector('#list').appendChild(p);
+
+}
+
+function handleDelete (e) {
+  e.target.parentNode.remove();
+}
